5bd28779fc12f2e7e02a9b765c65aac982e5fb8d
UrielSun <497819731@qq.com>
2019-10-24 14:12:39 +0800

    Site updated: 2019-10-24 14:12:39

--- photos/ins.js
+++ photos/ins.js
@@ -117,7 +117,7 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = baseUrl+ data.link[i]+"?imageView2/1/w/162";
+          var minSrc = baseUrl+ data.link[i]+"?imageView2/2/w/324";
           var src = baseUrl+ data.link[i];
           var type = data.type[i];
 
